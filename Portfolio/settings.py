705cc2db0848ce397a5069efb46ac6bb49615243
rahul <rahulrajt08@gmail.com>
2026-07-05 21:34:29 +0530

    migrating db

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -55,11 +55,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        os.environ.get(
+            "DATABASE_URL",
+            f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+        )
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
