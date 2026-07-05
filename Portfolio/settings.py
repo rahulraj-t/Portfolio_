3ff13557a141b58f984b2a36d0af4e5d659e9462
rahul <rahulrajt08@gmail.com>
2026-07-05 16:25:35 +0530

    changed form issue

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -84,8 +84,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 465
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 import os
 
